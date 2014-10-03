4dfdf291eac2c3e6c9f15449fa980b17ab018479
Carl Eriksson <carle293@hotmail.com>
2014-10-03 21:10:47 +0200

    Added persons list

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var config = {
     //Port that web server listens on
     SERVER_PORT: process.env.PORT || 9000
+
+    VIPS: ["Viktor Mattsson","Daniel Hollsten", "Carl Eriksson", "Magnus Gudmandsen"]
 }
 
 //Make it global
