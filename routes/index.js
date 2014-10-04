89c3d1ebc69d129b5af8af29771a3429c1738397
Carl Eriksson <carle293@hotmail.com>
2014-10-04 13:08:08 +0200

    More work on the index page

--- routes/index.js
+++ routes/index.js
@@ -27,85 +27,93 @@ exports.index = function(req, res){
 				}
 
 				lastId = docs[0]["id"];
+				console.log("Last id is: " + lastId);
   				// docs is an array containing documents Mars, Earth, Jupiter
   				// If no document is found, docs is equal to []
 			});
 		},
 		//Scrape all the that is missing
 		function(callback){
+
+
 				async.until(
-		//Condition
-		function(){
-			return data.length >= config.LIMIT ;
-		}
-		//Do this until the condition is true
-		,function(callback){
-		// console.log("Number of pages: " + kattisPageNumber);
-		var url = 'https://kth.kattis.com/submissions?page='+kattisPageNumber;
-		request(url, function (error, response, html){
-			if (!error && response.statusCode == 200) {
-				var $ = cheerio.load(html);
-					
-				var row = {};
-				var numberRow = 0;
-				
-				$('td').each(function(j,col){
-					var b = $(this);
-
-					var key = "ID";
-					var saveData = false;
-
-					switch(j-(7*numberRow)){
-						//index
-						case 0:
-							key = "id";
-							break;
-						//DATE
-						case 1:
-							key = "date";
-							break;
-						//AUTHOR
-						case 2:
-							key = "author";
-							break;
-						//PROBLEM
-						case 3:
-							key = "problem";
-							break;
-						//STATUS
-						case 4:
-							key = "status";
-							break;
-						//CPU
-						case 5:
-							key = "cpu";
-							break;
-						//LANG:
-						case 6: "value", 
-							key = "lang";
-							numberRow++;
-							saveData = true;
-							break;
+					//Condition
+					function(){
+						return data.length >= config.LIMIT || currentId > lastId;
 					}
-					row[key] = b.text();
+					//Do this until the condition is true
+					,function(callback){
+					// console.log("Number of pages: " + kattisPageNumber);
+					var url = 'https://kth.kattis.com/submissions?page='+kattisPageNumber;
+					request(url, function (error, response, html){
+						if (!error && response.statusCode == 200) {
+							var $ = cheerio.load(html);
+								
+							var row = {};
+							var numberRow = 0;
+							
+							$('td').each(function(j,col){
+								var b = $(this);
 
-					if(saveData){
-						var linenumber = numberRow;
-						//Check if this is person of interest
-						if(config.names[row["author"]]){
-							data.push(row);
+								var key = "ID";
+								var saveData = false;
+
+								switch(j-(7*numberRow)){
+									//index
+									case 0:
+										key = "id";
+										break;
+									//DATE
+									case 1:
+										key = "date";
+										break;
+									//AUTHOR
+									case 2:
+										key = "author";
+										break;
+									//PROBLEM
+									case 3:
+										key = "problem";
+										break;
+									//STATUS
+									case 4:
+										key = "status";
+										break;
+									//CPU
+									case 5:
+										key = "cpu";
+										break;
+									//LANG:
+									case 6: "value", 
+										key = "lang";
+										numberRow++;
+										saveData = true;
+										break;
+								}
+								row[key] = b.text();
+
+								//We just got our self a row of data
+								if(saveData){
+									//Check if this is person of interest
+									if(config.names[row["author"]]){
+										//Check if we done have it already
+										if(row["id"] > lastId){
+											//Insert it to the database
+
+										}
+									}	
+								}
+								//Empty the row
+								row = {};
+							});
+							kattisPageNumber++;
+							callback();
 						}
-						row = {};
-					}
-				});
-				kattisPageNumber++;
-				callback();
-			}
-			else {
-				callback(error);
-			}
-		});
-	},
+						else {
+							callback(error);
+						}
+					});
+				},
 	//Done send back to the user
 	function(err){
 		
